fb92162282348c355cad2c3f947948b38f6bd6fb
Мария <mariya_piterskaya@ymail.com>
2022-09-12 22:29:42 +0300

    Переменные 2. Задачи 1-2

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Задача 1");
+        int i = 1085429766;
+        byte b = 93;
+        short s = -27953;
+        long l = 854428782L;
+        float f = -953.5f;
+        double d = 387.7;
+
+        System.out.println("Значение переменной i с типом int равно " + i );
+        System.out.println("Значение переменной b с типом byte равно " + b );
+        System.out.println("Значение переменной s с типом short равно " + s );
+        System.out.println("Значение переменной l с типом long равно " + l );
+        System.out.println("Значение переменной f с типом float равно " + f );
+        System.out.println("Значение переменной d с типом double равно " + d );
+
+        System.out.println("Задача 2");//Задача 2
+        float boat = 27.12f;
+        long way = 987678965549L;
+        double whiskey = 2.786;
+        boolean isMore = whiskey > 5;
+        char symbol = 569;
+        short height = -159;
+        int dog = 27897;
+        byte cat = 67;
+        System.out.println(symbol); //проверка значения
+        System.out.println(isMore); //проверка значения
+    }
+}
